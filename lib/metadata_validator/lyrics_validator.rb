60d5da37dfd4a03c6034751c4e18e3b010dc588f
Monique Rio <nique.rio@gmail.com>
2018-09-29 11:06:53 -0400

    added lyrics_validator

---
+++ lib/metadata_validator/lyrics_validator.rb
@@ -0,0 +1,44 @@
+require 'csv'
+require_relative "./result"
+class LyricsValidator
+  attr_reader :repo_root, :slug
+  def initialize(repo_root:, slug:)
+    @repo_root = repo_root
+    @slug = slug
+    raise ArgumentError, 'directory or slug does not exist' unless File.directory?(directory) 
+  end
+  def validate
+    result = Result.new
+    if !File.exists?(lyrics_path)
+      return result
+    end
+
+    utf8 = `grep -axv '.*' #{lyrics_path}`
+    unless utf8.empty?
+       result.add_error('lyrics encoding not utf8') 
+       return result
+    end
+
+    begin
+      CSV.read(lyrics_path)      
+    rescue
+      result.add_error('lyrics have malformed csv')
+      return result
+    end
+    
+    last_line = `tail -1 #{lyrics_path}`
+    result.add_error('lyrics have empty rows at end') unless last_line =~ /[^\s,]/
+    
+    result
+  end
+
+  private
+  def directory
+    "#{@repo_root}/#{@slug}"
+  end
+
+  def lyrics_path
+    "#{directory}/lyrics.csv"
+  end
+  
+end
---
+++ spec/metadata_validator/lyrics_validator_spec.rb
@@ -0,0 +1,58 @@
+require_relative '../../lib/metadata_validator/lyrics_validator'
+
+describe LyricsValidator do
+  before(:each) do
+    @slug = 'slug'
+    @repo_root = "./"
+    Dir.mkdir("./slug")
+    FileUtils.copy('./spec/fixtures/good_lyrics.csv', './slug/lyrics.csv') 
+  end
+  after(:each) do
+    FileUtils.rm_r('./slug')
+  end
+  describe "initialize" do
+    it "accepts good input" do
+      v = LyricsValidator.new(repo_root: @repo_root, slug: @slug)
+      expect(v.slug).to eq(@slug)
+      expect(v.repo_root).to eq(@repo_root)
+    end 
+    it "raises error for non existent slug" do
+      expect{LyricsValidator.new(repo_root: @repo_root, slug: 'alewfiajew')}.to raise_error(ArgumentError, 'directory or slug does not exist')
+    end
+    it "raises error for non existent directory" do
+      expect{LyricsValidator.new(repo_root: './alwefjawe/', slug: @slug)}.to raise_error(ArgumentError, 'directory or slug does not exist')
+    end
+  end
+  describe "validate" do
+    def lv_factory(repo_root: @repo_root, slug: @slug) 
+      LyricsValidator.new(repo_root: repo_root, slug: slug) 
+    end
+    it "for good lyrics.csv returns successful result" do
+      lv = lv_factory
+      expect(lv.validate.successful?).to be_truthy
+    end
+    it "for nonexistent lyrics.csv returns successful result" do
+      File.delete('./slug/lyrics.csv')
+      lv = lv_factory
+      expect(lv.validate.successful?).to be_truthy
+    end 
+    it "rejects non utf8 encoding for lyrcs csv" do
+      FileUtils.cp 'spec/fixtures/bad_lyrics_encoding.csv', 'slug/lyrics.csv'
+      result = lv_factory.validate
+      expect(result.successful?).to be_falsey
+      expect(result.errors.first).to eq("lyrics encoding not utf8")
+    end
+    it "rejects lyrics csv with empty rows" do
+      FileUtils.cp 'spec/fixtures/bad_lyrics_extra_lines.csv', 'slug/lyrics.csv'
+      result = lv_factory.validate
+      expect(result.successful?).to be_falsey
+      expect(result.errors.first).to eq("lyrics have empty rows at end")
+    end
+    it "rejects lyrics csv malformed csv" do
+      FileUtils.cp 'spec/fixtures/bad_lyrics.csv', 'slug/lyrics.csv'
+      result = lv_factory.validate
+      expect(result.successful?).to be_falsey
+      expect(result.errors.first).to eq("lyrics have malformed csv")
+    end
+  end
+end
