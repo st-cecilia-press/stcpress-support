c84baadda36be79904e48707f1cf8f8f25db676a
Monique Rio <nique.rio@gmail.com>
2018-09-29 23:55:53 -0400

    Deleted old metadata_validators;

--- lib/validator/carol_metadata/check_metadata.rb
+++
@@ -1,6 +0,0 @@
-require_relative 'validate'
-
-input_file = ARGV[0]
-abort 'need yaml file' if input_file.nil?
-metadata = YAML.load_file(input_file)  
-puts validate_piece(metadata)
--- lib/validator/carol_metadata/spec/spec_helper.rb
+++
@@ -1,2 +0,0 @@
-require_relative '../validate.rb'
-require 'yaml'
